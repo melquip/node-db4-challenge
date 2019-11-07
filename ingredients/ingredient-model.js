970ba19f7a748871d178b84e15759c985f2be813
Melquisedeque Pereira <melqui1@live.com.pt>
2019-11-07 14:29:11 +0000

    Fixed bug with ingredients query

--- ingredients/ingredient-model.js
+++ ingredients/ingredient-model.js
@@ -1,10 +1,5 @@
 const db = require('../data/db-config');
 
-module.exports = {
-  getIngredient,
-  getRecipesByIngredientId,
-}
-
 const getIngredient = (id) => {
   return db('ingredient').where({ id });
 }
@@ -12,6 +7,11 @@ const getIngredient = (id) => {
 const getRecipesByIngredientId = (id) => {
   return db('recipe_ingredient AS ri')
     .join('recipe AS r', 'ri.recipe_id', 'r.id')
-    .select('ri.*')
+    .select('ri.recipe_id', 'r.recipe', 'ri.ingredient_id', 'ri.quantity')
     .where({ 'ri.ingredient_id': id });
+}
+
+module.exports = {
+  getIngredient,
+  getRecipesByIngredientId,
 }
