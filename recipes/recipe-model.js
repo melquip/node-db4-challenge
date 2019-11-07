2379fd3e2c7dddf3b1f88ed292b956ab3dd1f0e4
Melquisedeque Pereira <melqui1@live.com.pt>
2019-11-07 13:56:28 +0000

    Added getRecipes, getShoppingList and getInstructions to the data model

--- recipes/recipe-model.js
+++ recipes/recipe-model.js
@@ -1,27 +1,52 @@
 const db = require('../data/db-config');
 
 module.exports = {
-  find,
-  findById,
+  getRecipes,
+  getShoppingList,
+  getInstructions,
+  getRecipe,
   add,
   update,
   remove,
 }
+/*
+getRecipes(): 
+should return a list of all recipes in the database.
 
-const find = () => {
+getShoppingList(recipe_id): 
+should return a list of all ingredients and quantities for a given recipe
+
+getInstructions(recipe_id): 
+should return a list of step by step instructions for preparing a recipe
+*/
+const getRecipes = () => {
   return db('recipe');
 }
 
-const findById = (id) => {
+const getShoppingList = (id) => {
+  return db('recipe_ingredient AS ri')
+    .join('ingredient AS i', 'ri.ingredient_id', 'i.id')
+    .select(
+      'ri.recipe_id AS recipe_id, ' +
+      'i.ingredient AS ingredient, ' +
+      'ri.quantity'
+    ).where({ 'recipe_id': id });
+}
+
+const getInstructions = (id) => {
+  return db('recipe_step').where({ 'recipe_id': id });
+}
+
+const getRecipe = (id) => {
   return db('recipe').where({ id });
 }
 
-const add = (scheme) => {
-  return db('recipe').insert(scheme);
+const add = (recipe) => {
+  return db('recipe').insert(recipe);
 }
 
 const update = (changes, id) => {
-  return db('recipe').where({ id }).update(changes).then(() => findById(id));
+  return db('recipe').where({ id }).update(changes).then(() => getRecipe(id));
 }
 
 const remove = (id) => {
