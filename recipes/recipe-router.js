89f4fe1feb5bb83b09d023a47ec47e2b95097a78
Melquisedeque Pereira <melqui1@live.com.pt>
2019-11-07 12:58:41 +0000

    Updated package json

--- recipes/recipe-router.js
+++ recipes/recipe-router.js
@@ -6,26 +6,24 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   Recipes.find()
-  .then(recipes => {
-    res.json(recipes);
-  })
-  .catch(next);
+    .then(recipes => {
+      res.json(recipes);
+    })
+    .catch(next);
 });
 
 router.get('/:id', (req, res, next) => {
   const { id } = req.params;
 
   Recipes.findById(id)
-  .then(recipe => {
-    if (recipe) {
-      res.json(recipe);
-    } else {
-      res.status(404).json({ message: 'Could not find recipe with given id.' })
-    }
-  })
-  .catch(err => {
-    res.status(500).json({ message: 'Failed to get recipes' });
-  });
+    .then(recipe => {
+      if (recipe) {
+        res.json(recipe);
+      } else {
+        res.status(404).json({ message: 'Could not find recipe with given id.' })
+      }
+    })
+    .catch(next);
 });
 
 router.get('/:id/ingredients', (req, res, next) => {
@@ -47,28 +45,28 @@ router.post('/', (req, res, next) => {
   const recipeData = req.body;
 
   Recipes.add(recipeData)
-  .then(recipe => {
-    res.status(201).json(recipe);
-  })
-  .catch(next);
+    .then(recipe => {
+      res.status(201).json(recipe);
+    })
+    .catch(next);
 });
 
 router.post('/:id/steps', (req, res, next) => {
   const stepData = req.body;
-  const { id } = req.params; 
+  const { id } = req.params;
 
   Recipes.findById(id)
-  .then(recipe => {
-    if (recipe) {
-      Recipes.addStep(stepData, id)
-      .then(step => {
-        res.status(201).json(step);
-      })
-    } else {
-      res.status(404).json({ message: 'Could not find recipe with given id.' })
-    }
-  })
-  .catch(next);
+    .then(recipe => {
+      if (recipe) {
+        Recipes.addStep(stepData, id)
+          .then(step => {
+            res.status(201).json(step);
+          })
+      } else {
+        res.status(404).json({ message: 'Could not find recipe with given id.' })
+      }
+    })
+    .catch(next);
 });
 
 router.put('/:id', (req, res, next) => {
@@ -76,31 +74,31 @@ router.put('/:id', (req, res, next) => {
   const changes = req.body;
 
   Recipes.findById(id)
-  .then(recipe => {
-    if (recipe) {
-      Recipes.update(changes, id)
-      .then(updatedScheme => {
-        res.json(updatedScheme);
-      });
-    } else {
-      res.status(404).json({ message: 'Could not find recipe with given id' });
-    }
-  })
-  .catch(next);
+    .then(recipe => {
+      if (recipe) {
+        Recipes.update(changes, id)
+          .then(updatedScheme => {
+            res.json(updatedScheme);
+          });
+      } else {
+        res.status(404).json({ message: 'Could not find recipe with given id' });
+      }
+    })
+    .catch(next);
 });
 
 router.delete('/:id', (req, res, next) => {
   const { id } = req.params;
 
   Recipes.remove(id)
-  .then(deleted => {
-    if (deleted) {
-      res.json({ removed: deleted });
-    } else {
-      res.status(404).json({ message: 'Could not find recipe with given id' });
-    }
-  })
-  .catch(next);
+    .then(deleted => {
+      if (deleted) {
+        res.json({ removed: deleted });
+      } else {
+        res.status(404).json({ message: 'Could not find recipe with given id' });
+      }
+    })
+    .catch(next);
 });
 
 router.use((error, req, res, next) => {
